33c2d4f4fc2d94e219dbb30ad51691ade0c2c0f2
moistdio <92444580+moistdio@users.noreply.github.com>
2024-06-10 18:11:10 +0200

    Show import data

--- src/ui/menu-ui-handler.ts
+++ src/ui/menu-ui-handler.ts
@@ -121,7 +121,7 @@ export default class MenuUiHandler extends MessageUiHandler {
       });
     };
 
-    if (Utils.isLocal || !Utils.isLocal) {
+    if (Utils.isLocal) {
       manageDataOptions.push({
         label: i18next.t("menuUiHandler:importSession"),
         handler: () => {
@@ -152,7 +152,7 @@ export default class MenuUiHandler extends MessageUiHandler {
       },
       keepOpen: true
     });
-    if (Utils.isLocal) {
+    if (Utils.isLocal || !Utils.isLocal) {
       manageDataOptions.push({
         label: i18next.t("menuUiHandler:importData"),
         handler: () => {
